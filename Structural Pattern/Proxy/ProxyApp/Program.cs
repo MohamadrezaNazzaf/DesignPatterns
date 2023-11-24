43cac63598cf69c9787c6bf00e10855031cd7773
nazzaf <nazzafff@gmail.com>
2023-11-24 20:44:54 +0330

    add proxy pattern

---
+++ Structural Pattern/Proxy/ProxyApp/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace ProxyApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ISubject subject = new Proxy();
+
+        subject.DoAction();
+
+        Console.WriteLine("Hello World!");
+    }
+
+    public interface ISubject
+    {
+        void DoAction();
+    }
+
+    public class RealSubject : ISubject
+    {
+        public void DoAction()
+        {
+            Console.WriteLine("RealSubject.DoAction() is Run....");
+        }
+    }
+
+
+    public class Proxy : ISubject
+    {
+        private RealSubject RealSubject;
+
+        public void DoAction()
+        {
+            if (RealSubject == null)
+            {
+                RealSubject = new RealSubject();
+            }
+            RealSubject.DoAction();
+        }
+    }
+}
