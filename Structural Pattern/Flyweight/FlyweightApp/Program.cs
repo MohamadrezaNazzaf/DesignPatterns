2b42c500f1f3c4b141a11eb6f28e84969e37f3e0
nazzaf <nazzafff@gmail.com>
2023-12-04 22:02:17 +0330

    add flyweight pattern

--- Structural Pattern/Flyweight/FlyweightApp/Program.cs
+++ Structural Pattern/Flyweight/FlyweightApp/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FlyweightApp.UML;
+
+FlyweightFactory factory = new FlyweightFactory();
+
+var obj1 = factory.GetFlyweight("Test1");
+var obj2 = factory.GetFlyweight("Test2");
+var obj3 = factory.GetFlyweight("Test1");
+
+obj1.Operation("sample 1 extrisic state.");
+obj2.Operation("sample 2 extrisic state.");
+obj3.Operation("sample 3 extrisic state.");
+
+UnSharedConcreteFlyweight flyweights = new UnSharedConcreteFlyweight(new Flyweight[]
+{
+    factory.GetFlyweight("Test1"),
+    factory.GetFlyweight("Test2"),
+    factory.GetFlyweight("Test3"),
+    factory.GetFlyweight("Test4"),
+});
+
+flyweights.Operation("extrinsic ---> UnSharedConcreteFlyweight");
+
+Console.ReadKey();
---
+++ Structural Pattern/Flyweight/FlyweightApp/UML/Flyweight.cs
@@ -0,0 +1,55 @@
+﻿namespace FlyweightApp.UML;
+
+public abstract class Flyweight
+{
+    public abstract void Operation(string extrisicState);
+}
+
+class ConcreteFlyweight : Flyweight
+{
+    private string _intrisicState;
+    public ConcreteFlyweight(string intrisicState)
+    {
+        _intrisicState = intrisicState;
+    }
+
+    public override void Operation(string extrisicState)
+    {
+        Console.WriteLine($"ConcreteFlyweight.Operation({_intrisicState}) : \" {extrisicState} \" ");
+    }
+}
+
+class FlyweightFactory
+{
+    private Dictionary<string, Flyweight> flyweights = new Dictionary<string, Flyweight>();
+
+    public Flyweight GetFlyweight(string key)
+    {
+        Flyweight flyweight = null;
+        if (flyweights.TryGetValue(key, out flyweight)) { }
+        else
+        {
+            flyweights.Add(key, new ConcreteFlyweight(key));
+        }
+
+        return flyweights[key];
+    }
+}
+
+
+class UnSharedConcreteFlyweight : Flyweight
+{
+    private IEnumerable<Flyweight> _flyweights;
+    public UnSharedConcreteFlyweight(IEnumerable<Flyweight> flyweights)
+    {
+        _flyweights = flyweights;
+    }
+    public override void Operation(string extrisicState)
+    {
+        Console.WriteLine("* UnSharedConcreteFlyweight *");
+        foreach (var item in _flyweights)
+        {
+            item.Operation(extrisicState);
+        }
+    }
+}
