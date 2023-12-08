f1bc0917b5544b2b6f5f9d698b68c56e9fb59a8f
nazzaf <nazzafff@gmail.com>
2023-12-09 00:31:37 +0330

    add builder pattern

---
+++ Creational Pattern/Builder/BuilderApp/Classes/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace BuilderApp.Classes
+{
+    public class Product
+    {
+        public string Part1 { get; set; }
+        public string Part2 { get; set; }
+        public string Name { get; set; }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Product Name:{Name}");
+            Console.WriteLine($"Product Part1:{Part1}");
+            Console.WriteLine($"Product Part2:{Part2}");
+        }
+    }
+}
