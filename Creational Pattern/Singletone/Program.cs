9a41381df2116b2e924eed8311277b52bf999dcd
nazzaf <nazzafff@gmail.com>
2023-12-08 23:32:40 +0330

    add singletone pattern

---
+++ Creational Pattern/Singletone/Program.cs
@@ -0,0 +1,4 @@
+﻿var obj1 = Singletone.UML.Singletone.GetInstance();
+var obj2 = Singletone.UML.Singletone.GetInstance();
+
+Console.WriteLine(obj1 == obj2);
---
+++ Creational Pattern/Singletone/UML/Singletone.cs
@@ -0,0 +1,15 @@
+﻿namespace Singletone.UML;
+
+public class Singletone
+{
+    private static readonly Singletone uniqeInstance = new Singletone();
+
+    private Singletone()
+    {
+    }
+
+    public static Singletone GetInstance()
+    {
+        return uniqeInstance;
+    }
+}
