c5b3251654be8ff3fc4d34a2d8069f415bb6efc4
nazzaf <nazzafff@gmail.com>
2023-12-12 22:30:58 +0330

    add chain of responsibility pattern

---
+++ Behavioral Patterns/Chain of Responsibility/ChainOfResponsibilitiApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using ChainOfResponsibilitiApp.SnapExample;
+
+CreateOrder createOrder = new CreateOrder();
+SendOrderToDrivers sendOrderToDrivers = new SendOrderToDrivers();
+CheckUserActivity checkUserActivity = new CheckUserActivity();
+
+checkUserActivity
+    .SetSuccessor(createOrder)
+    .SetSuccessor(sendOrderToDrivers);
+
+checkUserActivity.Handle(new RequestContext
+{
+    UserId = 1,
+    Origin = new Point { Lat = 51.365, Lng = 25.145},
+    Destination = new Point { Lat = 14.658, Lng = 780}
+});
+
+Console.Read();
---
+++ Behavioral Patterns/Chain of Responsibility/ChainOfResponsibilitiApp/SnapExample/Snap.cs
@@ -0,0 +1,110 @@
+﻿
+using System.Security.Principal;
+
+namespace ChainOfResponsibilitiApp.SnapExample;
+
+
+public class RequestContext
+{
+    public int UserId { get; set; }
+    public Point Origin { get; set; }
+    public Point Destination { get; set; }
+}
+
+public class Point
+{
+    public double Lat { get; set; }
+    public double Lng { get; set; }
+}
+
+public class ResponseContext
+{
+    public bool IsSuccess { get; set; }
+    public string Message { get; set; }
+}
+
+public interface IHandler
+{
+    IHandler SetSuccessor(IHandler handler);
+
+    ResponseContext Handle(RequestContext request);
+}
+
+public abstract class TakeATaxiHandler : IHandler
+{
+    protected IHandler successor; // it is next step.
+
+    public abstract ResponseContext Handle(RequestContext request);
+
+    public IHandler SetSuccessor(IHandler handler)
+    {
+        this.successor = handler;
+        return successor;
+    }
+}
+
+public class CreateOrder : TakeATaxiHandler
+{
+    public override ResponseContext Handle(RequestContext request)
+    {
+        bool orderCreated = true;
+        if (orderCreated)
+        {
+            Console.WriteLine("order created.");
+
+            if (successor != null)
+                return successor.Handle(request); //send request to next step;
+        }
+
+        return new ResponseContext()
+        {
+            IsSuccess = false,
+            Message = "error eccurred in order create."
+        };
+    }
+}
+
+public class SendOrderToDrivers : TakeATaxiHandler
+{
+    public override ResponseContext Handle(RequestContext request)
+    {
+        Console.WriteLine("sended to drivers.");
+        if(successor != null)
+            return successor.Handle(request); //send request to next step;
+
+        return new ResponseContext()
+        {
+            IsSuccess = true,
+            Message = "order created and sended to drivers."
+        };
+    }
+}
+
+public class CheckUserActivity : TakeATaxiHandler
+{
+    public override ResponseContext Handle(RequestContext request)
+    {
+        if(request.UserId != 1)
+        {
+            Console.WriteLine("user not active");
+            return new ResponseContext
+            {
+                IsSuccess = false,
+                Message = "user not active"
+            };
+        }
+        else if(successor != null)
+        {
+            Console.WriteLine("user is active.");
+            return successor.Handle(request);
+        }
+        else
+        {
+            return new ResponseContext
+            {
+                IsSuccess = false,
+                Message = "There is no step for continue."
+            };
+        }
+    }
+}
