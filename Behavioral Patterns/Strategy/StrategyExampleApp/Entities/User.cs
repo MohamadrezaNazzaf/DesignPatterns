f12fec9bbe5d072b5f66e2db0c59230cbf109ec4
nazzaf <nazzafff@gmail.com>
2023-12-14 21:53:20 +0330

    add strategy pattern

---
+++ Behavioral Patterns/Strategy/StrategyExampleApp/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿namespace StrategyExampleApp.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public int Credit { get; set; }
+
+
+    public void DisplayUser()
+    {
+        Console.WriteLine($"Id: {Id} Name: {Name} LastName: {LastName} Credit:{Credit}");
+    }
+
+}
